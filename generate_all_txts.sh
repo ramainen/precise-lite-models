a85c6b9c6402bcff077f0b1bcf47c6f95566c33d
Damir Fakhrutdinov <ainu.sky@gmail.com>
2021-10-28 17:58:50 +0300

    Plus new models

--- generate_all_txts.sh
+++ generate_all_txts.sh
@@ -17,3 +17,16 @@ do
 
     done
 done
+
+
+echo "Generating plots..."
+
+for txtfile in ./txts/*.txt
+do
+  txt="$(basename -- $txtfile)"
+  txtname="${txt%.*}"
+  echo "${txt}..."
+  
+  gnuplot -e "set term png size 2000,308; set style line 11 lc rgb '#808080' lt 1; set border 3 back ls 11; set tics nomirror; set style line 12 lc rgb '#808080' lt 0 lw 1; set grid back ls 12; set style line 2 lc rgb '#5e9c36' pt 6 ps 1 lt 1 lw 2; set xlabel '${txtname}'; set ylabel 'y axis label'; set yrange [0:1]; set output './graphs/${txtname}.png' ; plot '${txtfile}' using 0:1 with linespoints  ls 2"
+  
+done
